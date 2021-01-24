d228bc244e022f5909b175d1790d963f5f1633bb
SudoPsych <77137933+SudoPsych@users.noreply.github.com>
2021-01-23 19:58:05 -0500

    Update Process Images.py
    Standardized the code so the class can be called as an object with an image parameter. The function to create the state data only runs if the
    file is ran directly. Also added linear regression.

--- Process Images.py
+++ Process Images.py
@@ -3,52 +3,49 @@
 Next to do:
 - Extract the data from the csv file and display with matplotlib
 - Tidy everything up into a neat GUI that displays all the images and graphs
+- Change the program to accept all different img sizes
+- Try out different fractals and country outlines
 """
 
 from PIL import Image, ImageStat, ImageChops
-import os
+from matplotlib import pyplot as plt
 from math import log
-import matplotlib.pyplot as plt
-import csv
+import os
 import time
-
+import csv
 
 
 class Fractal:
 
-    def __init__(self):
+    def __init__(self, img):
 
-
-        self.img_length = 2000
-        # Factors of 2000 for perfect grid
-        # Factors of 2000:
-        # 2, 4, 5, 8, 10, 16, 20, 25, 40, 50, 80, 100, 125, 200, 250, 400, 500, 1000
-        self.factors = [2, 4, 5, 8, 10, 16, 20, 25, 40, 50, 80, 100, 125, 200, 250]
+        self.steps = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
+        self.img = img
 
 
 
-
-    def split_image(self, img):
+    def split_image(self):
         # 50 States
         # Get state name
-        state_name = img.filename.split('.')[0]
-        img = img.convert('RGB')
+        state_name = self.img.filename.split('.')[0]
+        img = self.img.convert('RGB')
+        size = img.size
 
         img_data = []
         pictures = []
 
-        for factor in self.factors:
+        for factor in self.steps:
             # 10 diff images
             # Step is number of squares per row/column
             step = factor
             # Increment is number of pixels per step
-            incr = int(self.img_length / step)
+            incr = int(size[0] / step)
 
             outline_in_image = 0
 
             image_bits = []
             # Create blank image to paste bits onto
-            highlighted_image = Image.new('RGB', (2000, 2000))
+            highlighted_image = Image.new('RGB', size)
 
             for num in range(step ** 2):
                 # Image bits
@@ -88,12 +85,12 @@ class Fractal:
 
     def calculate_fractal_dimensions(self, img_data):
 
-        graph_data = [[],[],[]]
+        graph_data = [[],[],[], 0]
 
         for j in range(len(img_data) - 1):
 
             # Calculate change in block size between pictures
-            scaling_factor = self.factors[j + 1] / self.factors[j]
+            scaling_factor = self.steps[j + 1] / self.steps[j]
             scaling_factor = round(scaling_factor, 4)
             # Calculate change in number of highlighted blocks between pictures
             block_ratio = img_data[j + 1] / img_data[j]
@@ -107,9 +104,23 @@ class Fractal:
             graph_data[1].append(block_ratio)
             graph_data[2].append(fractal_dimension)
 
+        # Get log values of scaling factors and block ratios
+        log_s_f = [log(num) for num in graph_data[0]]
+        log_b_r = [log(num) for num in graph_data[1]]
+        # Calculate line of best fit
+        mean_s_f = sum(log_s_f) / len(log_s_f)
+        mean_b_r = sum(log_b_r) / len(log_b_r)
+
+        numerator = 0
+        denominator = 0
+        for factor, ratio in zip(log_s_f, log_b_r):
+            # Linear regression
+            numerator += ((factor - mean_s_f) * (ratio - mean_b_r))
+            denominator += ((factor - mean_s_f) ** 2)
+        best_fit = numerator / denominator
+        graph_data[3] = best_fit
 
         return graph_data
-
 
 
     def data_to_file(self, name, graph_data):
@@ -126,11 +137,24 @@ class Fractal:
         if os.path.getsize('picture_data.csv') == 0:
             write_f.writerow(['Name', 'Grid Size', 'Block Ratio', 'Scaling Factor', 'Fractal Dimension'])
         # The first image will only have a size so write that in
-        write_f.writerow([name_, self.factors[0] ** 2])
+        write_f.writerow([name_, self.steps[0] ** 2])
         # Write the data for each (other) image to the file
         for i in range(len(s_f)):
-            write_f.writerow(['', self.factors[i + 1] ** 2, b_r[i], s_f[i], f_d[i]])
+            write_f.writerow(['', self.steps[i + 1] ** 2, b_r[i], s_f[i], f_d[i]])
 
+
+    def create_plot(self, graph_data):
+        # Create the plots from the data
+
+        # Unpack the data
+        s_f, b_r, f_r = graph_data
+
+        log_s_f = [log(num) for num in s_f]
+        log_b_r = [log(num) for num in b_r]
+
+        plt.plot(log_s_f, log_b_r)
+
+        return
 
 
 
@@ -143,40 +167,51 @@ class Fractal:
 
         # Add each image of different grid size to respective state folder
         for num in range(len(pictures)):
-            pictures[num].save('Highlighted States/{}/{}_{}.png'.format(name, name, num))
+            pic_size = self.steps[num]
+            pictures[num].save('Highlighted States/{}/{}_{}.png'.format(name, name, pic_size))
 
 
 
+def main():
+    # Generate images and data for 50 States, store the data so this function won't need to be run
+    # every time the GUI is opened.
+    start_time = time.time()
 
-    def main(self):
-        # Duh main function hur dur
-        start_time = time.time()
+    # Get images from directory
+    states = os.listdir('thin_bin_outlines')
+    # Make directory for processed images
+    if not os.path.exists('Highlighted States'):
+        os.mkdir('Highlighted States')
+    # Remove previous data from csv file
+    open('picture_data.csv', 'w').close()
 
-        # Get images from directory
-        states = os.listdir('bin_outlines')
-        # Make directory for processed images
-        if not os.path.exists('Highlighted States'):
-            os.mkdir('Highlighted States')
-        # Remove previous data from csv file
-        open('picture_data.csv', 'w').close()
+    for state_name in states:
 
-        for state_name in states:
+        state_time1 = time.time()
 
-            # Open image for use
-            print(state_name.split('.')[0])
-            state_img = Image.open('bin_outlines/{}'.format(state_name))
-            # Process images and retrieve data
-            img_data, pictures = self.split_image(state_img)
-            # Do the fancy math
-            graph_data = self.calculate_fractal_dimensions(img_data)
-            # Write fancy math results to csv file
-            self.data_to_file(state_name, graph_data)
-            # Store processed images in neat directories
-            self.store_images(state_name, pictures)
+        # Open image for use
+        print(state_name.split('.')[0])
+        state_img = Image.open('thin_bin_outlines/{}'.format(state_name))
+        # Create object with image parameter
+        state_object = Fractal(state_img)
+        # Process images and retrieve data
+        img_data, pictures = state_object.split_image()
+        # Do the fancy math
+        graph_data = state_object.calculate_fractal_dimensions(img_data)
+        # Store processed images in neat directories
+        state_object.store_images(state_name, pictures)
+        # Store state data in csv file
+        state_object.data_to_file(state_name, graph_data)
 
-        end_time = time.time()
-        total_time = str(round((end_time - start_time), 2))
-        print('Time elapsed: {}'.format(total_time))
+        state_time2 = time.time()
+        print(str(round((state_time2 - state_time1), 2)))
+
+    # Calculate program run time
+    end_time = time.time()
+    min = int((end_time - start_time) // 60)
+    sec = round((end_time - start_time) % 60, 2)
+    print('Time elapsed: {} Minutes, {} seconds'.format(min, sec))
 
 
-Fractal().main()
+if __name__ == '__main__':
+     main()
