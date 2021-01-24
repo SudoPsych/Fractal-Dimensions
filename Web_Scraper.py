58ad450deabe00c3dbc3f9a942c1d926b5823cc2
SudoPsych <77137933+SudoPsych@users.noreply.github.com>
2021-01-23 20:03:44 -0500

    Update Web_Scraper.py
    Added a function to grab some outlines of countries from a website. Not great images tbh, should still look for an academic or other high quality database of country outlines.

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -11,25 +11,67 @@ import requests
 import os
 
 
-source = requests.get('https://gisgeography.com/state-outlines-blank-maps-united-states/').text
-soup = BeautifulSoup(source, 'lxml')
+class Scraper():
 
-outline_directory = '/Users/Connor/Desktop/State Outlines/'
-os.mkdir(outline_directory)
-os.chdir(outline_directory)
+    def us_states(self):
+        source = requests.get('https://gisgeography.com/state-outlines-blank-maps-united-states/').text
+        soup = BeautifulSoup(source, 'lxml')
 
-state_info = soup.find_all('span', class_='su-lightbox')
+        outline_directory = '/Users/Connor/Desktop/State Outlines/'
+        os.mkdir(outline_directory)
+        os.chdir(outline_directory)
 
-for state in state_info:
+        state_info = soup.find_all('span', class_='su-lightbox')
 
-    link = state['data-mfp-src']
-    name = os.path.basename(link)
-    name = name.split('-')[:-2]
-    name = ' '.join(name)
+        for state in state_info:
 
-    outline_image = open('{}.jpg'.format(name), 'wb')
-    outline_image.write(urllib.request.urlopen(link).read())
+            link = state['data-mfp-src']
+            name = os.path.basename(link)
+            name = name.split('-')[:-2]
+            name = ' '.join(name)
 
-    print(name)
-    print(link)
-    print('')
+            outline_image = open('{}.jpg'.format(name), 'wb')
+            outline_image.write(urllib.request.urlopen(link).read())
+
+            print(name)
+            print(link)
+            print('')
+
+    def countries(self):
+
+        source = requests.get('https://www.worldatlas.com/countries').text
+        soup = BeautifulSoup(source, 'lxml')
+
+        outline_directory = '/Users/Connor/Desktop/Country Outlines/'
+        os.mkdir(outline_directory)
+        os.chdir(outline_directory)
+
+        recognized_countries = soup.find('ol')
+        country_names = recognized_countries.find_all('h3', id='item_name')
+        country_names = [name.get_text() for name in country_names]
+
+        no_image = []
+
+        for name in country_names:
+
+            new_name = name.replace(' ','-').lower()
+            print(new_name)
+
+            country_source = requests.get('https://www.worldatlas.com/maps/{}#outline1Section'.format(new_name)).text
+            country_soup = BeautifulSoup(country_source, 'lxml')
+
+            outline_div = country_soup.find('div', class_="map_outline_split_media")
+
+            try:
+                links = outline_div.find_all('a', class_='print_link')
+                for i in range(len(links)):
+                    link = links[i]['href']
+                    link = 'https://www.worldatlas.com{}'.format(link)
+                    print(link)
+                    outline_image = open('{}{}.png'.format(name,i), 'wb')
+                    outline_image.write(urllib.request.urlopen(link).read())
+            except:
+                no_image.append(name)
+
+        print(no_image)
+
