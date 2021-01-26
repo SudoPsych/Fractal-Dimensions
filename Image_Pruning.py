e87265928f7c4548c2004762f8dc38f080d8a8bf
SudoPsych <77137933+SudoPsych@users.noreply.github.com>
2021-01-25 19:19:18 -0500

    Update Image_Pruning.py
    Removed temporary code since these are permanent functions now. Added some descriptive comments and (mostly) generalized the functions.

--- Image_Pruning.py
+++ Image_Pruning.py
@@ -1,89 +1,43 @@
 """
-Temporary code used to refine the images
 1/10/21
 """
 
 import os
-from PIL import Image, ImageChops,ImageStat
+from PIL import Image
 import time
 from skimage.morphology import skeletonize
 import numpy
-from Process_Images import Fractal
 
-def image():
-
-    texas = Image.open('State Outlines/Texas.jpg')
-    os.chdir('outlines/bits')
-    texas.show()
-
-    # incr: increment of pixels
-    # step: total number of increments along one dimension
-    # x1 = (num % step) * incr
-    # x2 = x1 + incr
-    # y1 = (num // step) * incr
-    # y2 = y1 + incr
-
-    step = 5
-    incr = 200 / step
-
-    for num in range(step ** 2):
-
-        x1 = (num % step) * incr
-        x2 = x1 + incr
-        y1 = (num // step) * incr
-        y2 = y1 + incr
-
-        chad_bit = texas.crop((x1,y1,x2,y2))
-
-        chad_bit.save('bit_{}.jpg'.format(num))
-        print(ImageStat.Stat(chad_bit).stddev)
-    return
-
-
-def deleteBits():
-
-    os.chdir('outlines/bits')
-    bits = os.listdir()
-    for bit in bits:
-        os.remove(bit)
-
-def outlines():
-
-    os.chdir("outlines")
-    states = os.listdir()
-    for state in states:
-        state = Image.open(state)
-
-        if 2000 not in state.size:
-            print(state.filename)
-            print(state.size)
-    return
 
 def resize(img, size):
+    # Change image to be given size
 
     img = Image.open(img)
+    # Get length of image sides
     major_length = max(img.size)
     minor_length = min(img.size)
+    # Get index i.e. vertical or horizontal
     major_index = img.size.index(major_length)
     minor_index = img.size.index(minor_length)
-
+    # Value to scale image by
     multiplier = size / major_length
+    # New image lengths
     new_major = int(multiplier * major_length)
     new_minor = int(multiplier * minor_length)
-    print(new_minor)
-    print(new_major)
+    # Uhhhh
     new_size = [0, 0]
+    # Set new image lengths
     new_size[major_index] = new_major
     new_size[minor_index] = new_minor
     new_size = tuple(new_size)
-
+    # Make image a square (based on major length) and return it
     new_img = Image.new('RGB', (size, size), color=(255, 255, 255))
     new_img.paste(img, (int((size - new_size[0]) / 2),
                         int((size - new_size[1]) / 2)))
     return new_img
 
 
-def booleanize(img):
+def convert_to_binary(img):
     # Convert all the pixels in the image to either black or white
 
     time1 = time.time()
@@ -93,19 +47,18 @@ def booleanize(img):
 
     for x in range((img.size)[0]):
         for y in range((img.size)[0]):
-
+            # Loop through each pixel
             count += 1
             print(count)
-
+            # Get average value of pixel from RGB bands
             pixel = img.getpixel((x,y))
-
             avg = (pixel[0] + pixel[1] + pixel[2]) / 3
-
+            # Check if pixel is relatively black or white and sets absolute value
             if avg < 150:
                 new_img.putpixel((x,y), (0))
             else:
                 new_img.putpixel((x,y), (1))
-
+    # Uhhhhh fix this
     name = os.path.basename(img.filename)
     name = name.split('.')[0]
     print(name)
@@ -116,27 +69,34 @@ def booleanize(img):
     return
 
 
-def thinning(img):
+def skelotnize_(img_path):
+    # Turn ugly fat lines into easy to work with thin ones
 
     time1 = time.time()
     print('...')
-    img_path = img
-    img = Image.open(img)
+    img = Image.open(img_path)
     size = img.size
     thinned_img = Image.new('1', size)
     image_list = [[] for _ in range(size[0])]
 
     for x in range(size[0]):
         for y in range(size[0]):
+            # Loop through each pixel in image
+            # Turn image into nested list of values
             pixel = img.getpixel((x,y))
-            image_list[x].append(True if pixel == 0 else False)
-
+            # Invert colors as well (fuckin' love if/else statements with boolean values)
+            image_list[x].append(False if pixel else True)
+    # Convert to array
     image_array = numpy.array(image_list)
+    # Runs algorithm from handy dandy library idk wtf happens here tbh
+    # Removes excess pixels from outline while preserving it's relative shape/roughness
     skeleton = skeletonize(image_array)
+    # Back to list
     thinned_image = [list(array) for array in skeleton]
 
     for x in range(size[0]):
         for y in range(size[0]):
+            # Create new, skeletonized image. (Re-invert colors)
             thinned_img.putpixel((x,y), not thinned_image[x][y])
 
     thinned_img.save(img_path)
@@ -145,16 +105,4 @@ def thinning(img):
     total_time = str(round((time2 - time1), 2))
     print('Time elapsed: {}'.format(total_time))
     return
-
-
-path = "/Users/Connor/PycharmProjects/Random Projects/Fractal Dimensions/serp.png"
-tri = Image.open(path)
-triangle = Fractal(tri)
-data, pics = triangle.split_image()
-numbas = triangle.calculate_fractal_dimensions(data)
-
-for pic in pics:
-    pic.show()
-print(numbas[2])
-print(numbas[3])
 
