e969c84d8c96f557a9dbc63bbcb54cb8826500db
SudoPsych <77137933+SudoPsych@users.noreply.github.com>
2021-02-10 20:37:40 -0500

    Update Fractal_GUI.py
    Added dropdown menu
    Update menu in upload button function
    
    Buttons now declared in __init__
    
    Ready to add matplotlib display

--- Fractal_GUI.py
+++ Fractal_GUI.py
@@ -7,16 +7,18 @@ from tkinter.filedialog import askopenfilename
 from PIL import ImageTk
 from Process_Images import Fractal
 from Image_Pruning import *
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 
 root = Tk()
 
 class GUI():
 
-    def __init__(self, root):
+    def __init__(self, root_):
 
         self._Fractal = Fractal()
-        self.root = root
+        self.root = root_
         self.root.title('Fractal Dimension')
         # Create central canvas to display images
         self.canvas = Canvas(self.root, height = 500, width = 500)
@@ -24,12 +26,31 @@ class GUI():
         self.img = None
         # Path to image that is modifiable
         self.c_s_dir = 'Custom'
-        self.img_dir = 'binary'
+        self.img_dir = 'original'
         self.img_name = 'serp'
         self.current_img = 'image_data/Custom/original/serp.png'
         self.chosen_path = ''
         self.highlighted_index = 0
         self.highlighted_list = [pic for pic in os.listdir('image_data/Custom/highlighted/serp')]
+
+        self.original     = Button(self.root, text="original",    width=11)
+        self.binary       = Button(self.root, text="Binary",      width=11)
+        self.skeleton     = Button(self.root, text="Skeleton",    width=11)
+        self.choose       = Button(self.root, text="Choose File", width=11)
+        self.previous     = Button(self.root, text="<",           width=3)
+        self.next_        = Button(self.root, text=">",           width=3)
+        self.highlighted  = Button(self.root, text="Highlighted", width=11)
+        self.test         = Button(self.root, text="Test",        width=11)
+        self.upload       = Button(self.root, text="Upload",      width=11)
+
+        self.clicked = StringVar(self.root)
+        self.clicked.set('serp')
+        self.img_options = [name.split('.')[0] for name in os.listdir('image_data/Custom/binary')]
+        self.dropdown = OptionMenu(self.root,
+                                   self.clicked,
+                                   *self.img_options,
+                                   command=lambda clicked=self.clicked: self.dropdown_command(clicked))
+
         return
 
 
@@ -45,6 +66,7 @@ class GUI():
         self.img = ImageTk.PhotoImage(img)
         self.canvas.create_image(0,0, anchor=NW, image=self.img)
         return
+
 
 
 
@@ -109,11 +131,6 @@ class GUI():
         except:
             return
 
-        new_root = Tk()
-        label = Label(new_root, text='Hi :)')
-        label.pack()
-        new_root.mainloop()
-
         # Get name of image file
         name = os.path.basename(self.chosen_path).split('.')[0]
 
@@ -125,45 +142,66 @@ class GUI():
 
         # Resize image to 2000 x 2000 and convert to png file
         # Store in 'original' dir
+
         resized_image = resize_(self.chosen_path)
         resized_image.save(original_path)
         print('resize complete')
+
         # Convert to binary and save to 'binary' dir
         bin_image = convert_to_binary(original_path)
         bin_image.save(bin_path)
         print('bin complete')
+
         # Skeletonize the image and save in skeleton dir
         skeleton_image = skeletonize_(bin_path)
         skeleton_image.save(skeleton_path)
         print('skele complete')
+        
         # Split image up and save pictures to highlighted dir
         img_data, pictures = self._Fractal.split_image(bin_path, skeleton_path)
         self._Fractal.store_images(pictures, name)
         print('images stored')
+
         # Do fancy math and store data in csv file to proper dir
         graph_data = self._Fractal.calculate_fractal_dimensions(img_data)
         self._Fractal.data_to_file(csv_file_path, graph_data)
         print('data stored')
+
+        # Update the option menu
+        self.dropdown.destroy()
+        self.img_options.append(name)
+        self.dropdown = OptionMenu(self.root,
+                                   self.clicked,
+                                   *self.img_options,
+                                   command=lambda clicked=self.clicked: self.dropdown_command(clicked))
+        self.dropdown.grid(row=1, column=0)
+
+        self.c_s_dir = 'Custom'
+        self.img_dir = 'original'
+        self.img_name = name
+        self.current_img = original_path
+        self.put_image(self.current_img)
+
         return
 
 
 
 
-    def choose_file_command(self, butt):
+    def choose_file_command(self):
         # Allow user to choose file from computer
 
         # Gets user input
         self.chosen_path = askopenfilename()
         # Set text on button to the name of that file
         name = os.path.basename(self.chosen_path)
-        butt['text'] = name
+        self.choose['text'] = name
         return
 
 
 
 
     def test_command(self):
-        print(self.img_dir)
+
         pass
 
 
@@ -177,63 +215,41 @@ class GUI():
 
 
 
-    def create_buttons(self):
+    def place_widgets(self):
         # duh buttons :) smile
 
-        original     = Button(self.root, text="original",    width=11)
-        binary       = Button(self.root, text="Binary",      width=11)
-        skeleton     = Button(self.root, text="Skeleton",    width=11)
-        choose       = Button(self.root, text="Choose File", width=11)
-        previous     = Button(self.root, text="<",           width=3)
-        next_        = Button(self.root, text=">",           width=3)
-        highlighted  = Button(self.root, text="Highlighted", width=11)
-        test         = Button(self.root, text="Test",        width=11)
-        upload       = Button(self.root, text="Upload",      width=11)
+        self.original.grid(   row=1, column=5)
+        self.binary.grid(     row=2, column=5)
+        self.skeleton.grid(   row=3, column=5)
+        self.choose.grid(     row=6, column=2)
+        self.previous.grid(   row=0, column=3)
+        self.next_.grid(      row=0, column=4)
+        self.highlighted.grid(row=4, column=5)
+        self.test.grid(       row=5, column=5)
+        self.upload.grid(     row=6, column=1)
 
-        original.grid(   row=1, column=5)
-        binary.grid(     row=2, column=5)
-        skeleton.grid(   row=3, column=5)
-        choose.grid(     row=6, column=2)
-        previous.grid(   row=0, column=3)
-        next_.grid(      row=0, column=4)
-        highlighted.grid(row=4, column=5)
-        test.grid(       row=5, column=5)
-        upload.grid(     row=6, column=1)
+        self.original['command']    = lambda dir_name = 'original'  : self.change_dir(dir_name)
+        self.binary['command']      = lambda dir_name = 'binary'    : self.change_dir(dir_name)
+        self.skeleton['command']    = lambda dir_name = 'skeleton'  : self.change_dir(dir_name)
+        self.choose['command']      = self.choose_file_command
+        self.previous['command']    = lambda pre      = 'pre'       : self.pre_nex(pre)
+        self.next_['command']       = lambda nex      = 'nex'       : self.pre_nex(nex)
+        self.highlighted['command'] = self.highlighted_command
+        self.test['command']        = self.test_command
+        self.upload['command']      = self.upload_command
 
-        original['command']    = lambda dir_name = 'original'  : self.change_dir(dir_name)
-        binary['command']      = lambda dir_name = 'binary'    : self.change_dir(dir_name)
-        skeleton['command']    = lambda dir_name = 'skeleton'  : self.change_dir(dir_name)
-        choose['command']      = lambda butt     = choose      : self.choose_file_command(butt)
-        previous['command']    = lambda pre      = 'pre'       : self.pre_nex(pre)
-        next_['command']       = lambda nex      = 'nex'       : self.pre_nex(nex)
-        highlighted['command'] = self.highlighted_command
-        test['command']        = self.test_command
-        upload['command']      = self.upload_command
+        self.dropdown.grid(row=1, column=0)
 
         return
 
 
 
-    def create_dropdown(self):
-
-        img_options = os.listdir('image_data/Custom/binary')
-        img_options = [name.split('.')[0] for name in img_options]
-
-        clicked = StringVar()
-        clicked.set('serp')
-        dropdown = OptionMenu(self.root,
-                              clicked,
-                              *img_options,
-                              command= lambda clicked = clicked : self.dropdown_command(clicked))
-        dropdown.grid(row=1, column=0)
-
 
     def main(self):
 
         self.put_image(self.current_img)
-        self.create_buttons()
-        self.create_dropdown()
+        self.place_widgets()
         self.root.mainloop()
 
-
-GUI(root).main()
+if __name__ == '__main__':
+    GUI(root).main()
