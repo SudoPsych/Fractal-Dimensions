5ea9c8ae36df6d7264ccf5734d191851ad40286a
SudoPsych <77137933+SudoPsych@users.noreply.github.com>
2021-02-14 23:39:16 -0500

    Update Process_Images.py
    - Allow split_image to take not just factors of 2000 as input
    - Checks for outlines as white instead of black since skeletonized images are inverted now
    - Write to csv file with newline = '' so no blank lines
    - get_data converts strings to floats
    - Removed create_plot, thats the GUI's job
    - Changed any concatenations to .format
    - Fixed and ran the main function!

--- Process_Images.py
+++ Process_Images.py
@@ -6,20 +6,20 @@ GUI but it works fine this way as well.
 """
 
 from PIL import Image, ImageStat, ImageChops
-from matplotlib import pyplot as plt
-import numpy as np
+from Image_Pruning import *
 from math import log
 import os
 import time
 import csv
+from math import ceil
 
 
 class Fractal:
 
-    def __init__(self):
 
+    def __init__(self):
         # Nice even factors of 2000
-        self.steps = [10, 16, 20, 25, 40, 50, 80, 100, 125, 200]
+        self.steps = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
 
 
 
@@ -42,7 +42,7 @@ class Fractal:
             # Step is number of squares per row/column
             step = factor
             # Increment is number of pixels per step
-            incr = 2000 // step
+            incr = ceil(2000 / step)
 
             outline_in_image = 0
 
@@ -57,23 +57,26 @@ class Fractal:
                 y1 = (num // step) * incr
                 y2 = y1 + incr
 
-                # Crop image into small bit, add to list
-                display_bit = display_img.crop((x1,y1,x2,y2))
-                skeleton_bit = skeleton_img.crop((x1,y1,x2,y2))
+                if x1 <= 2000 or y1 <= 2000:
+                    # Crop image into small bit, add to list
+                    display_bit = display_img.crop((x1,y1,x2,y2))
+                    skeleton_bit = skeleton_img.crop((x1,y1,x2,y2))
 
-                # Check if outline is in that bit
-                min_value = ImageStat.Stat(skeleton_bit).extrema[0][0]
+                    # Check if outline is in that bit
+                    min_value = ImageStat.Stat(skeleton_bit).extrema[0][1]
 
-                if min_value == 0:
-                    # If outline is in image, tint its color yellow and save value
-                    outline_in_image  += 1
+                    if min_value == 255:
+                        # If outline is in image, tint its color yellow and save value
+                        outline_in_image  += 1
 
-                    image_tint = Image.new('RGB', (incr, incr), (255, 0, 0))
-                    display_bit = ImageChops.multiply(display_bit, image_tint)
+                        image_tint = Image.new('RGB', (incr, incr), (255, 0, 0))
+                        display_bit = ImageChops.multiply(display_bit, image_tint)
 
-                # Reconstructs the original image with the new bits (some are tinted now)
-                highlighted_image.paste(display_bit, (x1, y1))
+                    # Reconstructs the original image with the new bits (some are tinted now)
+                    highlighted_image.paste(display_bit, (x1, y1))
 
+            # Crop away excess
+            highlighted_image.crop((0,0,2000,2000))
             # Store images in list
             pictures.append(highlighted_image)
             # Stores outline ratio data
@@ -97,7 +100,7 @@ class Fractal:
             scaling_factor = self.steps[j + 1] / self.steps[j]
             scaling_factor = round(scaling_factor, 4)
             # Calculate change in number of highlighted blocks between pictures
-            block_ratio = img_data[j + 1] / img_data[j]
+            block_ratio = img_data[j+1] / img_data[j]
             block_ratio = round(block_ratio, 4)
             # Use b_r and s_f with some fancy log math to find f_d
             # This line of code is the focal point of the whole project
@@ -131,10 +134,12 @@ class Fractal:
     def data_to_file(self, path, graph_data):
         # Store state data in csv file
 
+
         # Extract data from list
         s_f, b_r, f_d, m, b = graph_data
         # Open csv file for storing this data
-        f = open(path, 'a')
+        f = open(path, 'a', newline = '')
+        f.truncate(0)
         write_f = csv.writer(f)
         # Scaling Factors
         write_f.writerow(['Scaling Factors'] + [num for num in s_f])
@@ -153,87 +158,91 @@ class Fractal:
     def get_data(self, file_path):
         # Get data from csv file
 
+        # Open file from given path
         f = open(file_path, newline='')
         read_f = csv.reader(f, delimiter=',')
+
+        # Read each line in succession
         s_f = read_f.__next__()[1:]
         b_r = read_f.__next__()[1:]
         f_d = read_f.__next__()[1:]
         m   = read_f.__next__()[1]
         b   = read_f.__next__()[1]
 
+        # Convert all values from integers to strings
+        s_f = [float(num) for num in s_f]
+        b_r = [float(num) for num in b_r]
+        f_d = [float(num) for num in f_d]
+        m   = float(m)
+        b   = float(b)
+
         graph_data = [s_f, b_r, f_d, m, b]
+
         return graph_data
 
 
-    def create_plot(self, graph_data):
-        # Create the plots from the data
-
-        # Unpack the data
-        s_f, b_r, f_d, m, b  = graph_data
-        # Turning lists into numpy arrays cuz matplotlib is picky
-        s_f = np.array(s_f)
-        b_r = np.array(b_r)
-        # Axes
-        plt.xlabel('log(Scaling Factor)')
-        plt.ylabel('log(Block Ratio)')
-        # Scatter plot for values and line of best fit
-        plt.scatter(s_f, b_r, label = "Calculated Values", color = 'b')
-        plt.plot(s_f, (m * s_f) + b, label = "Line of best fit", color = 'r')
-
-        plt.legend()
-        plt.show()
-
-        return
 
 
-
-    def store_images(self, pictures, name):
+    def store_images(self, pictures, name, c_or_s):
 
         # Check if dir exists first
-        if not os.path.exists('image_data/Custom/highlighted/' + name):
-            os.mkdir('image_data/Custom/highlighted/' + name)
+        if not os.path.exists("image_data/{}/highlighted/{}".format(c_or_s, name)):
+            os.mkdir("image_data/{}/highlighted/{}".format(c_or_s, name))
 
         # Add each image of different grid size to its designated folder
         for num in range(len(pictures)):
-            pictures[num].save('image_data/Custom/highlighted/{}/{}_{}.png'.format(name, name, num))
+            pictures[num].save('image_data/{}/highlighted/{}/{}_{}.png'.format(c_or_s, name, name, num))
 
 
 
 
 def main():
-    # Generate images and data for 50 States, store the data so this function won't need to be run
-    # every time the GUI is opened.
-
-    # Fix this code!
+    # Generate images and data for 50 States and store the data
 
     start_time = time.time()
 
     # Get images from directory
-    states = os.listdir('image_data/States/skeleton')
-    # Make directory for processed images
-    if not os.path.exists('image_data/States/highlighted'):
-        os.mkdir('image_data/States/highlighted')
-    # Remove previous data from csv file
-    open('picture_data.csv', 'w').close()
+    states_path = input("Enter the path to the directory of state images: ")
+    if os.path.exists(states_path):
+       states = [(states_path + name) for name in os.listdir(states_path)]
+    else:
+        return
 
-    for state_name in states:
+    for state in states:
 
         state_time1 = time.time()
 
-        print(state_name.split('.')[0])
-        # Get path to thick and thin image for processing
-        bin_path = 'image_data/states/binary/{}'.format(state_name)
-        thin_bin_path = 'image_data/states/skeleton/{}'.format(state_name)
-        # Create object with image parameter
+        # Get state name
+        state_name = os.path.basename(state).split('.')[0]
+
+        print(state_name)
+
+        # Create paths to directories
+        og_path = "image_data/States/original/{}.png".format(state_name)
+        bin_path = 'image_data/states/binary/{}.png'.format(state_name)
+        skeleton_path = 'image_data/states/skeleton/{}.png'.format(state_name)
+        csv_path = "image_data/states/csv_data/{}.csv".format(state_name)
+        # Create object to use functions above
         _Fractal = Fractal()
+        # Resize the state
+        resized_state = resize_(state)
+        resized_state.save(og_path)
+
+        # Convert image to binary
+        binary_image = convert_to_binary(og_path)
+        binary_image.save(bin_path)
+
+        # Skelotonize Image
+        skeleton_image = skeletonize_(bin_path)
+        skeleton_image.save(skeleton_path)
         # Process images and retrieve data
-        img_data, pictures = _Fractal.split_image(bin_path, thin_bin_path)
+        img_data, pictures = _Fractal.split_image(bin_path, skeleton_path)
         # Do the fancy math
         graph_data = _Fractal.calculate_fractal_dimensions(img_data)
         # Store processed images in neat directories
-        _Fractal.store_images(state_name, pictures)
+        _Fractal.store_images(pictures, state_name, "States")
         # Store state data in csv file
-        _Fractal.data_to_file(state_name, graph_data)
+        _Fractal.data_to_file(csv_path, graph_data)
 
         state_time2 = time.time()
         print(str(round((state_time2 - state_time1), 2)))
@@ -243,6 +252,8 @@ def main():
     min = int((end_time - start_time) // 60)
     sec = round((end_time - start_time) % 60, 2)
     print('Time elapsed: {} Minutes, {} seconds'.format(min, sec))
+
+    return
 
 
 if __name__ == '__main__':
